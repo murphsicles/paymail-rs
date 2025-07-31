d4f97d80ed4e82921833c1f28753c5f469c02f1e
Roy Murphy <roycoins@gmail.com>
2025-07-31 09:41:02 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 #![doc = "A fast, asynchronous Rust library for the BSV PayMail protocol."]
 
 pub mod client;
-pub mod utils;
 pub mod errors;
 pub mod models;
 pub mod protocols;
 pub mod resolver;
 pub mod server;
+pub mod utils;
 
 pub use client::PaymailClient;
 pub use errors::PaymailError;
