6d433b04fb84ed4628cdcf0af55b923e095df712
Roy Murphy <roycoins@gmail.com>
2025-07-31 08:21:02 +0100

    Update pki.rs

--- src/protocols/pki.rs
+++ src/protocols/pki.rs
@@ -3,5 +3,3 @@ use crate::{PaymailClient, PaymailError};
 pub async fn fetch_pubkey(client: &PaymailClient, paymail: &str) -> Result<String, PaymailError> {
     client.get_pubkey(paymail).await
 }
-
-// TODO: Add verification if needed
