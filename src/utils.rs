746af14a60c9f6c17f4f2531592e3f8848575c27
Roy Murphy <roycoins@gmail.com>
2025-08-03 05:24:50 +0100

    Update utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -7,13 +7,16 @@ use sv::script::Script;
 
 pub fn generate_signature(priv_key: &SecretKey, message: &str) -> Result<String, PaymailError> {
     let hash = ring::digest::digest(&SHA256, message.as_bytes());
-    let msg =
-        Message::from_digest(hash.as_ref()).map_err(|e| PaymailError::Other(e.to_string()))?;
+    let msg_array: [u8; 32] = hash
+        .as_ref()
+        .try_into()
+        .map_err(|_| PaymailError::Other("Invalid hash length".to_string()))?;
+    let msg = Message::from_digest(msg_array);
     let secp = Secp256k1::new();
-    let recoverable_sig = secp.sign_ecdsa_recoverable(msg, priv_key);
+    let recoverable_sig = secp.sign_ecdsa_recoverable(&msg, priv_key);
     let (recovery_id, compact) = recoverable_sig.serialize_compact();
     let mut full_sig = [0u8; 65];
-    full_sig[0] = 31 + recovery_id.as_i32() as u8;
+    full_sig[0] = 31 + recovery_id.to_i32() as u8;
     full_sig[1..].copy_from_slice(&compact);
     Ok(base64::engine::general_purpose::STANDARD.encode(full_sig))
 }
@@ -41,18 +44,21 @@ pub fn verify_signature(
             "Invalid recovery header".to_string(),
         ));
     }
-    let recovery_id = ecdsa::RecoveryId::from((header - 31) as i32)
+    let recovery_id = ecdsa::RecoveryId::try_from((header - 31) as i32)
         .map_err(|e| PaymailError::Other(e.to_string()))?;
     let compact_sig = ecdsa::RecoverableSignature::from_compact(&sig_bytes[1..], recovery_id)
         .map_err(|e| PaymailError::Other(e.to_string()))?;
     let standard_sig = compact_sig.to_standard();
 
     let hash = ring::digest::digest(&SHA256, message.as_bytes());
-    let msg =
-        Message::from_digest(hash.as_ref()).map_err(|e| PaymailError::Other(e.to_string()))?;
+    let msg_array: [u8; 32] = hash
+        .as_ref()
+        .try_into()
+        .map_err(|_| PaymailError::Other("Invalid hash length".to_string()))?;
+    let msg = Message::from_digest(msg_array);
 
     let secp = Secp256k1::new();
-    Ok(secp.verify_ecdsa(msg, &standard_sig, &pub_key).is_ok())
+    Ok(secp.verify_ecdsa(&msg, &standard_sig, &pub_key).is_ok())
 }
 
 pub fn parse_script(hex_str: &str) -> Result<Script, PaymailError> {
