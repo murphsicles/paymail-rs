ee794ab5c3f5332c79c174ea838f32bad836fba1
Roy Murphy <roycoins@gmail.com>
2025-08-08 17:58:43 +0100

    Update resolver.rs

--- src/resolver.rs
+++ src/resolver.rs
@@ -14,29 +14,24 @@ impl Resolver for DefaultResolver {
     async fn resolve_host(&self, domain: &str) -> Result<(String, u16), PaymailError> {
         let resolver =
             TokioAsyncResolver::tokio(ResolverConfig::default(), ResolverOpts::default());
-        let srv_query = format!("_bsvalias._tcp.{domain}");
-        if let Ok(srv) = resolver.srv_lookup(srv_query).await {
-            if let Some(record) = srv.iter().next() {
-                let target = record
-                    .target()
-                    .to_string()
-                    .trim_end_matches('.')
-                    .to_string();
-                return Ok((target, record.port()));
-            }
+        let srv_query = format!("_bsvalias._tcp.{}", domain);
+        if let Ok(srv) = resolver.srv_lookup(&srv_query).await && let Some(record) = srv.iter().next() {
+            let target = record
+                .target()
+                .to_string()
+                .trim_end_matches('.')
+                .to_string();
+            return Ok((target, record.port()));
         }
-        if let Ok(a_lookup) = resolver.ipv4_lookup(domain).await {
-            if let Some(ip) = a_lookup.iter().next() {
-                return Ok((ip.to_string(), 443));
-            }
+        if let Ok(a_lookup) = resolver.ipv4_lookup(domain).await && let Some(ip) = a_lookup.iter().next() {
+            return Ok((ip.to_string(), 443));
         }
-        if let Ok(aaaa_lookup) = resolver.ipv6_lookup(domain).await {
-            if let Some(ip) = aaaa_lookup.iter().next() {
-                return Ok((ip.to_string(), 443));
-            }
+        if let Ok(aaaa_lookup) = resolver.ipv6_lookup(domain).await && let Some(ip) = aaaa_lookup.iter().next() {
+            return Ok((ip.to_string(), 443));
         }
         Err(PaymailError::DnsFailure(format!(
-            "No host found for {domain}"
+            "No host found for {}",
+            domain
         )))
     }
 }
